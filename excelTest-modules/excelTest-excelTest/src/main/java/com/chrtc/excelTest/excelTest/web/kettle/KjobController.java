dbaf5b299fb7d6a586908cc8797f06f81d136447
zhangyi <zycy6699@163.com>
2018-11-16 18:28:45 +0800

    11

--- excelTest-modules/excelTest-excelTest/src/main/java/com/chrtc/excelTest/excelTest/web/kettle/KjobController.java
+++ excelTest-modules/excelTest-excelTest/src/main/java/com/chrtc/excelTest/excelTest/web/kettle/KjobController.java
@@ -444,7 +444,7 @@ public class KjobController {
         }
         //读取磁盘文件
         String content = FileUtils.readFileToString(loginFile, Constant.DEFAULT_ENCODING);
-        String[] split = content.split("\\r\\n");
+        String[] split = content.split("\\n");
         return ResultFactory.create(split);
     }
 
